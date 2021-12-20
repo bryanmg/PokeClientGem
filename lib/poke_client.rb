b8b354f6153fc26433404f2ec6d433e5caea171a
bryan <bmendoza5@ucol.mx>
2021-12-20 13:24:58 -0600

    Initial commit

---
+++ lib/poke_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "poke_client/version"
+
+module PokeClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/poke_client/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PokeClient
+  VERSION = "0.1.0"
+end
