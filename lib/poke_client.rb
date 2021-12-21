a5e3c102a8ee51e4706d6ecaaa7371fab6d2b482
bryan <bmendoza5@ucol.mx>
2021-12-20 23:08:08 -0600

    Create initial interface of gem

--- lib/poke_client.rb
+++ lib/poke_client.rb
@@ -1,8 +1,54 @@
 # frozen_string_literal: true
 
+require "json"
 require_relative "poke_client/version"
+require_relative "poke_client/client"
+require "net/http"
 
 module PokeClient
   class Error < StandardError; end
-  # Your code goes here...
+  # def get(url, options={})
+  #   uri = uri_path(url)
+  #   if options[:query_params].is_present?
+  #     uri.query = URI.encode_www_form(options[:query_params])
+  #   end
+  #   return Net::HTTP.get(uri)
+  # end
+
+  
+  def self.all
+    PokeClient::Client.new.all("pokemon")
+  end
+
+  def self.normal
+    PokeClient::Client.new.all("type/1")
+  end
+
+  def self.fighting
+    PokeClient::Client.new.all("type/2")
+  end
+
+  def self.flying
+    PokeClient::Client.new.all("type/3")
+  end
+
+  def self.ground
+    PokeClient::Client.new.all("type/5")
+  end
+
+  def self.rock
+    PokeClient::Client.new.all("type/6")
+  end
+
+  def self.fire
+    PokeClient::Client.new.all("type/10")
+  end
+
+  def self.water
+    PokeClient::Client.new.all("type/11")
+  end
 end
+
+e = PokeClient
+puts e
+#binding.irb
---
+++ test/poke_client_test.rb
@@ -0,0 +1,62 @@
+#frozen_string_literal: true
+
+#require "test_helper"
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "poke_client"
+require "minitest/autorun"
+#require "vcr"
+
+class PokeClientTest < Minitest::Test
+    def test_taht_it_has_version_number
+        refute_nil ::PokeClient::VERSION
+    end
+
+    def test_fetc_all_pokemons
+        pokemons = PokeClient.all
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+
+    def test_fetch_all_normal_pokemons
+        pokemons = PokeClient.normal
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+
+    def test_fetch_all_rock_pokemons
+        pokemons = PokeClient.rock
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+
+    def test_fetch_all_fighting_pokemons
+        pokemons = PokeClient.fighting
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+
+    def test_fetch_all_flying_pokemons
+        pokemons = PokeClient.flying
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+
+    def test_fetch_all_ground_pokemons
+        pokemons = PokeClient.ground
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+
+
+    def test_fetch_all_fire_pokemons
+        pokemons = PokeClient.fire
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+
+    def test_fetch_all_water_pokemons
+        pokemons = PokeClient.water
+        assert_instance_of Array, pokemons
+        assert_instance_of Hash, pokemons.first
+    end
+end
